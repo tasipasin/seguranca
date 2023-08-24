d61d0e6729077aaac067388041c191e95630dc79
tasi.pasin <tasi.pasin96@gmail.com>
2023-08-24 20:01:27 -0300

    - Alteração da classe e criação da tela.

--- src/main/java/com/tasi/LoginController.java
+++ src/main/java/com/tasi/LoginController.java
@@ -1,25 +1,25 @@
 package com.tasi;
-/*
-Put header here
-
-
- */
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-public class FXMLController implements Initializable {
+public class LoginController implements Initializable {
     
     @FXML
-    private Label lblOut;
+    private Button loginBtn;
+    
+    @FXML
+    private Label errorLabel;
     
     @FXML
     private void btnClickAction(ActionEvent event) {
-        lblOut.setText("Hello World!");
+        errorLabel.setText("tiririca");
     }
     
     @Override
